18ba3cb4b57c34e1c2004fb8218efa7e9300ecee
0xirvan <darknet1255@gmail.com>
2025-01-27 02:36:45 +0700

    feat: refactor task reading functions and add ShowCompletedTasks

--- 01-todo-list/tasks/tasks.go
+++ 01-todo-list/tasks/tasks.go
@@ -4,6 +4,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"strconv"
 	"text/tabwriter"
 	"time"
 
@@ -17,26 +18,43 @@ type Tasks struct {
 	IsCompleted bool
 }
 
-func ReadAllTasks() {
+func ReadFile() ([][]string, error) {
 	// Open the file
 	file, err := os.Open("db/db.csv")
 	if err != nil {
-		fmt.Println("Error:", err)
-		return
+		return nil, err
 	}
 	defer file.Close()
-
-	// Print the records in a table
-	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.Debug)
-	defer w.Flush()
 
 	// Read the file
 	data := csv.NewReader(file)
 	records, err := data.ReadAll()
 	if err != nil {
+		return nil, err
+	}
+	return records, nil
+}
+
+func timeDiff(createdAt string) (string, error) {
+	time, err := time.Parse(time.RFC3339, createdAt)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return "", err
+	}
+	return timediff.TimeDiff(time), nil
+}
+
+func ShowAllTask() {
+	// Read the file
+	records, err := ReadFile()
+	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
+
+	// Create a new tabwriter
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.Debug)
+	defer w.Flush() // Flush the writer
 
 	// Write the records to the writer
 	for i, record := range records {
@@ -48,17 +66,52 @@ func ReadAllTasks() {
 		}
 
 		if i > 0 {
-			createdAt, err := time.Parse(time.RFC3339, record[2])
+			timeDiff, err := timeDiff(record[2])
 			if err != nil {
 				fmt.Println("Error:", err)
 				return
 			}
-			timeDiff := timediff.TimeDiff(createdAt)
 			fmt.Fprintf(w, "%s\t%s\t%s\t%s\n", record[0], record[1], timeDiff, record[3])
 		} else {
 			fmt.Fprintf(w, "%s\t%s\t%s\t%s\n", record[0], record[1], record[2], record[3])
 		}
 
+	}
+
+}
+
+func ShowCompletedTasks() {
+	// Read the file
+
+	records, err := ReadFile()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// Create new tabwriter
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.Debug)
+	defer w.Flush() // Flush the writer
+
+	// Write the records to the writer
+	for i, record := range records {
+		if len(record) < 4 {
+			fmt.Println("Invalid record:", record)
+			continue
+		}
+
+		if i > 0 {
+			if r, err := strconv.ParseBool(record[3]); err == nil && r {
+				timeDiff, err := timeDiff(record[2])
+				if err != nil {
+					fmt.Println("Error:", err)
+					return
+				}
+				fmt.Fprintf(w, "%s\t%s\t%s\n", record[0], record[1], timeDiff)
+			}
+		} else {
+			fmt.Fprintf(w, "%s\t%s\t%s\n", record[0], record[1], record[2])
+		}
 	}
 
 }
